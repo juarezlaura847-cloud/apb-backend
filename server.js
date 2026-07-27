89e044dcb009bb401fab2a717b54b29fe423b18a
laurz <juarezlaura847@gmail.com>
2026-07-27 12:03:26 -0600

    Subiendo backend completo

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 let memoryStore = {
   updatedAt: Date.now(),
