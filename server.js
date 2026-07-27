ee7ec5a87c5b4e0a439b54859a6c081e80913be6
laurz <juarezlaura847@gmail.com>
2026-07-27 12:38:29 -0600

    Corregir inicialización de Express

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 
 let memoryStore = {
